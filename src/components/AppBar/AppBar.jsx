8f2ab35bf56c441a418aafaeb3435ba2a3264f0f
rasinoleg <rasinoleg3@gmail.com>
2023-10-02 21:52:00 +0300

    react 8-phonebook

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
-import { AuthNav } from 'components/AuthNav/AuthNav';
+import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks/useAuth';
 import css from './AppBar.module.css';
 
