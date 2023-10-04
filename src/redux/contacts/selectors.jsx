f22c420ff8f6c27b36517b1b80013c47c90353e4
rasinoleg <rasinoleg3@gmail.com>
2023-10-04 12:06:12 +0300

    react 8-phonebook

--- src/redux/contacts/selectors.jsx
+++
@@ -1,5 +0,0 @@
-export const selectLoading = state => state.contacts.isloading;
-
-export const selectFilter = state => state.filter;
-
-export const selectAllContacts = state => state.contacts.items;
