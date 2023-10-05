fd2d8ef52adf7b48870209c0e5957f1b78ce056d
rasinoleg <rasinoleg3@gmail.com>
2023-10-05 11:21:34 +0300

    react 8-phonebook

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,9 +7,14 @@ export const Navigation = () => {
 
   return (
     <nav>
-      <NavLink className={css.link} to="/Home">
+      <NavLink className={css.link} to="/home">
         Home
       </NavLink>
+      {!isLoggedIn && (
+        <NavLink className={css.link} to="/login">
+          
+        </NavLink>
+      )}
       {isLoggedIn && (
         <NavLink className={css.link} to="/contacts">
           Contacts
@@ -18,3 +23,27 @@ export const Navigation = () => {
     </nav>
   );
 };
+
+
+
+
+// import { NavLink } from 'react-router-dom';
+// import { useAuth } from 'hooks/useAuth';
+// import css from './Navigation.module.css';
+
+// export const Navigation = () => {
+//   const { isLoggedIn } = useAuth();
+
+//   return (
+//     <nav>
+//       <NavLink className={css.link} to="/Home">
+//         Home
+//       </NavLink>
+//       {isLoggedIn && (
+//         <NavLink className={css.link} to="/contacts">
+//           Contacts
+//         </NavLink>
+//       )}
+//     </nav>
+//   );
+// };
