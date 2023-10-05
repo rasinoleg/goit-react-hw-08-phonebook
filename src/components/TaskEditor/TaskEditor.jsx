5cff10f642ac7d73b7b9bfc93c4773ceb2809358
rasinoleg <rasinoleg3@gmail.com>
2023-10-05 09:52:35 +0300

    react 8-phonebook

--- src/components/TaskEditor/TaskEditor.jsx
+++ src/components/TaskEditor/TaskEditor.jsx
@@ -1,11 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContacts } from 'redux/contacts/operations';
-import { fetchContacts } from 'redux/contacts/operations'; 
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectAllContacts } from 'redux/contacts/selectors';
 
 import css from './TaskEditor.module.css';
 
 export const TaskEditor = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(selectAllContacts);
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -14,9 +16,19 @@ export const TaskEditor = () => {
     const number = form.elements.number.value;
 
     if (name !== '' && number !== '') {
-      await dispatch(addContacts({ name, number }));
-      form.reset();
-      dispatch(fetchContacts());
+      
+      const isContactExists = contacts.some(contact => 
+        contact.name.toLowerCase() === name.toLowerCase() || 
+        contact.number === number
+      );
+
+      if (isContactExists) {
+        alert('This contact already exists.');
+      } else {
+        await dispatch(addContacts({ name, number }));
+        form.reset();
+        dispatch(fetchContacts());
+      }
      
       return;
     }
@@ -47,36 +59,5 @@ export const TaskEditor = () => {
 
 
 
-// import { useDispatch } from 'react-redux';
-// import { addContacts } from 'redux/contacts/operations';
-// import css from './TaskEditor.module.css';
-
-// export const TaskEditor = () => {
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-//     const name = form.elements.text.value;
-//     const number = form.elements.number.value; 
-
-//     if (name !== '' && number !== '') { 
-//       dispatch(addContacts({ name, number })); 
-//       form.reset();
-//       return;
-//     }
-//     alert('Task and number cannot be empty. Enter some text and a number!');
-//   };
-
-//   return (
-//     <form className={css.form} onSubmit={handleSubmit}>
-//       <input name="text" type="text"className={css.input} placeholder="Name" />
-//       <input name="number" type="tel" className={css.inputNumber} placeholder="Number" /> 
-//       <button type="submit" className={css.button}>
-//         Add Contacts
-//       </button>
-//     </form>
-//   );
-// };
 
 
